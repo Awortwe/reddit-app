21d3ace92187c388266c79eb2fa208494b134a26
Awortwe <enockawor@gmail.com>
2023-02-06 07:52:09 -0800

    migrating community database

--- database/migrations/2023_02_06_154624_create_communities_table.php
+++ database/migrations/2023_02_06_154624_create_communities_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('communities', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
